760758ccdb668df9b16f11fad81cfc5a47fb54eb
illescasDaniel <illescas.daniel@protonmail.com>
2018-07-12 23:05:08 +0200

    Added missing extensions

--- CachedImages.swift
+++ CachedImages.swift
@@ -169,3 +169,31 @@ class CachedImages {
 		}
 	}
 }
+
+extension UIImage {
+	
+	convenience init?(contentsOf url: URL?) {
+		
+		guard let validURL = url else { return nil }
+		
+		if let imageData = try? Data(contentsOf: validURL) {
+			self.init(data: imageData)
+			return
+		}
+		return nil
+	}
+	
+	static func manageContentsOf(_ url: URL?, completionHandler: @escaping ((UIImage, URL?) -> ()), errorHandler: (() -> ())? = nil) {
+		DispatchQueue.global().async {
+			if let validImage = UIImage(contentsOf: url) {
+				DispatchQueue.main.async {
+					completionHandler(validImage, url)
+				}
+			} else {
+				DispatchQueue.main.async {
+					errorHandler?()
+				}
+			}
+		}
+	}
+}
